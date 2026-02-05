24898050074c924c4850a2ff16fda1c8fac4cbe9
Theodore <tl_uk@icloud.com>
2026-02-05 22:45:14 +0000

    Phase 5.3 debugging routes visualisation.

--- ui/tabs/__init__.py
+++ ui/tabs/__init__.py
@@ -12,7 +12,8 @@ from .network_tab import render_network_tab
 from .infrastructure_tab import render_infrastructure_tab
 from .scenario_report_tab import render_scenario_report_tab
 from .combined_scenarios_tab import render_combined_scenarios_tab
-from .environmental_tab import render_environmental_tab  # FIX: Added this!
+from .environmental_tab import render_environmental_tab
+from .analytics_tab import render_analytics_tab
 
 __all__ = [
     'render_map_tab',
